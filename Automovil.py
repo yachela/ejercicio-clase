e8a4962e36595bb7b07e700b72f46abdc81c0c07
yachela <laura.yachelini@davinci.edu.ar>
2025-05-09 20:07:46 -0300

    Punto 4 del ejercicio

--- Automovil.py
+++ Automovil.py
@@ -1,9 +1,12 @@
 class Automovil:
+    def __init__(self, color, marca, aceleracion, velocidad):
+        self.ruedas = 4
+        self.color = color
+        self.marca = marca
+        self.aceleracion = aceleracion
+        self.velocidad = velocidad
 
-def __init__(self, ruedas, color, marca, aceleracion, velocidad)
-self.ruedas = 4
-self.color = color
-self.marca = marca
-self.aceleracion = aceleracopm
-self.velocidad = velocidad
-
+mi_bm = Automovil("Azul", "BMW", 2.6, 150)
+    
+print(f"Numero de ruedas: {mi_bm.ruedas}")
+print(f"Aceleración de: {mi_bm.aceleracion}")
