a72089f4aa3e980449c1103a6040ae2b04d1f651
yachela <laura.yachelini@davinci.edu.ar>
2025-05-09 20:14:41 -0300

    Creacion de metodos acelerar, frenar y creacion de segundo objeto

--- Automovil.py
+++ Automovil.py
@@ -6,7 +6,21 @@ class Automovil:
         self.aceleracion = aceleracion
         self.velocidad = velocidad
 
+    def acelerar(self):
+        self.aceleracion = 3
+        self.velocidad = 150
+
+    def frenar(self):
+    self.aceleracion = 0
+    self.velocidad = 0
+
 mi_bm = Automovil("Azul", "BMW", 2.6, 150)
+mi_audi = Automovil("Negro", "Audi", 2.9, 180)
     
 print(f"Numero de ruedas: {mi_bm.ruedas}")
 print(f"Aceleración de: {mi_bm.aceleracion}")
+
+mi_bm.aceleracion = 2.9;
+print(f"Aceleración de: {mi_bm.aceleracion}")
+
+mi_audi.frenar
