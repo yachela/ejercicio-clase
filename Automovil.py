163ddbf9ecb014074cb458ecd7675e4bfe0fb1eb
yachela <laura.yachelini@davinci.edu.ar>
2025-05-09 19:58:51 -0300

    First commit

---
+++ Automovil.py
@@ -0,0 +1,9 @@
+class Automovil:
+
+def __init__(self, ruedas, color, marca, aceleracion, velocidad)
+self.ruedas = 4
+self.color = color
+self.marca = marca
+self.aceleracion = aceleracopm
+self.velocidad = velocidad
+
